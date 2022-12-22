823c3a95a9ed490192d2435a5780a7f1c438d363
Noel Dasco Jr <119291271+v88-noel@users.noreply.github.com>
2022-12-22 17:04:42 +0800

    Update messagesSlice.js

--- src/redux/messagesSlice.js
+++ src/redux/messagesSlice.js
@@ -9,41 +9,41 @@ export const messagesSlice = createSlice({
         comment_info_to_delete: {message_id: 0, comment_id: 0}
     },
     reducers: {
-        addMessage:(state, action)=>{
+        addMessage:(state, { payload })=>{
             const message_id = new Date().valueOf();
             state.message = {...state.message,
-                                [message_id]:{message_text: action.payload, comments:{}}};
+                                [message_id]:{message_text:  payload , comments:{}}};
         },
-        deleteMessage:(state, action)=>{
-            delete state.message[action.payload];
+        deleteMessage:(state, {payload})=>{
+            delete state.message[payload];
         },
-        editMessage:(state, action)=>{
-            const message_id = action.payload.message_id;
-            const new_message_text = action.payload.new_message_text;
+        editMessage:(state, {payload})=>{
+            const message_id = payload.message_id;
+            const new_message_text = payload.new_message_text;
             state.message[message_id].message_text = new_message_text;
         },
-        editComment:(state, action)=>{
-            const comment_id = action.payload.comment_id;
-            const message_id = action.payload.message_id;
-            const new_comment_text = action.payload.new_comment_text;
+        editComment:(state, {payload})=>{
+            const comment_id = payload.comment_id;
+            const message_id = payload.message_id;
+            const new_comment_text = payload.new_comment_text;
             state.message[message_id].comments[comment_id] = new_comment_text;
         },
-        addComment:(state, action)=>{
+        addComment:(state, {payload})=>{
             const comment_id = new Date().valueOf();
-            const comment_text = action.payload.comment_text;
-            const message_id = action.payload.message_id;
+            const comment_text = payload.comment_text;
+            const message_id = payload.message_id;
             state.message[message_id].comments[comment_id] = comment_text;
         },
-        deleteComment:(state, action)=>{
-            const message_id = action.payload.message_id;
-            const comment_id = action.payload.comment_id;
+        deleteComment:(state,{payload})=>{
+            const message_id = payload.message_id;
+            const comment_id = payload.comment_id;
             delete state.message[message_id].comments[comment_id];
         },
-        setMessageToDelete:(state, action)=>{
-            state.message_id_to_delete = action.payload;
+        setMessageToDelete:(state,{payload})=>{
+            state.message_id_to_delete = payload;
         },
-        setCommentToDelete:(state, action)=>{
-            state.comment_info_to_delete = {message_id: action.payload.message_id, comment_id: action.payload.comment_id};
+        setCommentToDelete:(state, {payload})=>{
+            state.comment_info_to_delete = {message_id: payload.message_id, comment_id: payload.comment_id};
         }
     },
 });
